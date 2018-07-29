0c13b1e4b0ec2e7e5060e580af3a6a1222403f97
Catherine Wise <catherine.wise@smartward.com.au>
2018-07-29 16:02:52 +1000

    Move css + script

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+	let data = [
+		{url: "https://google.com", title: "Google", description: "Search the web", image: "https://images.unsplash.com/photo-1531752074002-abf991376d04"},
+		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
+		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
+		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
+		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
+		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
+		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
+		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
+		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
+	];
+
+	let preview = document.getElementById("preview-frame");
+	let list = document.getElementById("link-list");
+	let template = document.getElementById("list-template");
+	data.forEach(x => {
+		let newElem = document.importNode(template.content, true);
+		newElem.querySelector("a").href = x.url;
+		newElem.querySelector(".link-text").textContent = x.title;
+		newElem.querySelector(".description").textContent = x.description;
+		newElem.querySelector("img").src = x.image;
+		list.appendChild(newElem);
+	});
+});
