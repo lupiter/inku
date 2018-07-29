2ea85f228b4139660c7d6bf64a57cb6975c30c5e
Catherine Wise <catherine.wise@smartward.com.au>
2018-07-29 17:17:06 +1000

    Switch to js-yaml, support fetching file

--- script.js
+++ script.js
@@ -1,53 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
-	let yamlString = `
-- url: https://google.com
-  title: Google
-  description: Search the web
-  image: https://images.unsplash.com/photo-1531752074002-abf991376d04
-- url: https://github.com
-  title: Github
-  description: Sharing code
-  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
-- url: https://github.com
-  title: Github
-  description: Sharing code
-  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
-- url: https://github.com
-  title: Github
-  description: Sharing code
-  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
-- url: https://github.com
-  title: Github
-  description: Sharing code
-  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
-- url: https://github.com
-  title: Github
-  description: Sharing code
-  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
-- url: https://github.com
-  title: Github
-  description: Sharing code
-  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
-- url: https://github.com
-  title: Github
-  description: Sharing code
-  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
-- url: https://github.com
-  title: Github
-  description: Sharing code
-  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
-`
+function addItem(template, item, list) {
+	let newElem = document.importNode(template.content, true);
+	newElem.querySelector("a").href = item.url;
+	newElem.querySelector(".link-text").textContent = item.title;
+	newElem.querySelector(".description").textContent = item.description;
+	newElem.querySelector("img").src = item.image;
+	list.appendChild(newElem);
+}
 
-	let preview = document.getElementById("preview-frame");
+function loadYaml(template, list, yamlString) {
+	list.innerHTML = '';
+	let data = jsyaml.load(yamlString);
+	data.forEach(x => {
+		addItem(template, x, list);
+	});
+}
+
+function fetchYaml(url, template, list) {
+	fetch(url).then(x => x.text()).then(x => {
+		loadYaml(template, list, x);
+	});
+}
+
+document.addEventListener('DOMContentLoaded', function() {
 	let list = document.getElementById("link-list");
 	let template = document.getElementById("list-template");
-	let data = nativeObject = YAML.parse(yamlString);
-	data.forEach(x => {
-		let newElem = document.importNode(template.content, true);
-		newElem.querySelector("a").href = x.url;
-		newElem.querySelector(".link-text").textContent = x.title;
-		newElem.querySelector(".description").textContent = x.description;
-		newElem.querySelector("img").src = x.image;
-		list.appendChild(newElem);
+	let form = document.getElementById("upload-form");
+	let edit = document.getElementById("edit-button");
+	let editCancel = document.getElementById("cancel-button");
+	let editSave = document.getElementById("save-button");
+	let urlField = document.getElementById("url-field");
+	edit.onclick = (e => { 
+		edit.style.display = 'none';
+		form.style.display = 'flex';
 	});
+	editCancel.onclick = (e => {
+		edit.style.display = 'inline';
+		form.style.display = 'none';
+	});
+	editSave.onclick = (e => {
+		fetchYaml(urlField.value, template, list);
+	})
+	fetchYaml("https://www.dropbox.com/s/st98mzqgvsdho9t/books.yaml?raw=1", template, list);
 });
