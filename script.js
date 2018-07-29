2f5d3203841f29ffe8a9f27b66ea3095f622f33d
Catherine Wise <catherine.wise@smartward.com.au>
2018-07-29 16:08:31 +1000

    Switch to YAML

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
-	let data = [
-		{url: "https://google.com", title: "Google", description: "Search the web", image: "https://images.unsplash.com/photo-1531752074002-abf991376d04"},
-		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
-		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
-		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
-		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
-		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
-		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
-		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
-		{url: "https://github.com", title: "Github", description: "Sharing code", image: "https://images.unsplash.com/photo-1496200186974-4293800e2c20"},
-	];
+	let yamlString = `
+- url: https://google.com
+  title: Google
+  description: Search the web
+  image: https://images.unsplash.com/photo-1531752074002-abf991376d04
+- url: https://github.com
+  title: Github
+  description: Sharing code
+  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
+- url: https://github.com
+  title: Github
+  description: Sharing code
+  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
+- url: https://github.com
+  title: Github
+  description: Sharing code
+  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
+- url: https://github.com
+  title: Github
+  description: Sharing code
+  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
+- url: https://github.com
+  title: Github
+  description: Sharing code
+  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
+- url: https://github.com
+  title: Github
+  description: Sharing code
+  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
+- url: https://github.com
+  title: Github
+  description: Sharing code
+  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
+- url: https://github.com
+  title: Github
+  description: Sharing code
+  image: https://images.unsplash.com/photo-1496200186974-4293800e2c20
+`
 
 	let preview = document.getElementById("preview-frame");
 	let list = document.getElementById("link-list");
 	let template = document.getElementById("list-template");
+	let data = nativeObject = YAML.parse(yamlString);
 	data.forEach(x => {
 		let newElem = document.importNode(template.content, true);
 		newElem.querySelector("a").href = x.url;
