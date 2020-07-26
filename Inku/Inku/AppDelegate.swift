f962082d88d004421f0f6bd7896c882008b4a04e
Catherine Wise <everbloom@gmail.com>
2020-07-26 16:16:47 +1000

    More

---
+++ Inku/Inku/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Inku
+//
+//  Created by Catherine Wise on 14/9/18.
+//  Copyright © 2018 cathywise. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Inku/Inku/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Inku
+//
+//  Created by Catherine Wise on 14/9/18.
+//  Copyright © 2018 cathywise. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
